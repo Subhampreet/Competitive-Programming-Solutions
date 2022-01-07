a2e0140f462595eb2142a0a0042e3d3d1f76de7d
Subhampreet Mohanty <subhampreet18@gmail.com>
2022-01-08 01:23:48 +0530

    Jan 2022 : LeetCode 66

---
+++ LeetCode/066-Plus_One.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        for(int i = n -1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            
+            digits[i] = 0;
+        }
+        
+        int[] new_number = new int[n+1];
+        new_number[0] = 1;
+        return new_number;
+    }
+}
