29a904ffccf5c133d28d8bd9a3f27f538a67e318
Subhampreet Mohanty <subhampreet18@gmail.com>
2022-01-28 21:21:32 +0530

    Jan 2022 : LeetCode 53

---
+++ LeetCode/053-Maximum_Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            
+            if(sum > max) max = sum;
+            if(sum < 0) sum = 0;
+        }
+        return max;
+    }
+}
