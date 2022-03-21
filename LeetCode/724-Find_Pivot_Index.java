a0592198f70ff858e2241ba434e53ba8bf698b83
Subhampreet Mohanty <subhampreet18@gmail.com>
2022-03-21 16:21:04 +0530

    DAY-1 : LeetCode 724 Update

--- LeetCode/724-Find_Pivot_Index.java
+++ LeetCode/724-Find_Pivot_Index.java
@@ -1,17 +1,18 @@
+// Solution : https://www.youtube.com/watch?v=0PiH6Beqif8
+
 class Solution {
     public int pivotIndex(int[] nums) {
-        for(int i = 0; i < nums.length; i++ ){
-            int sum1 = 0;
-            int sum2 = 0;
-            for( int j = i-1; j >= 0; j-- ){
-                sum1 = sum1 +  nums[j];
-            }
-            for( int k = i+1; k < nums.length; k++ ){
-                sum2 = sum2 +  nums[k];
-            }
-            if( sum1 == sum2 ){
+        int total_sum = 0;
+        for(int i =0; i < nums.length; i++){
+            total_sum += nums[i];
+        }
+        int left_sum = 0;
+        for(int i = 0; i < nums.length ; i++)
+        {
+            if(i != 0 ) left_sum += nums[i-1];
+            if(total_sum - left_sum - nums[i] == left_sum)
                 return i;
-            }
+            
         }
         return -1;
     }
