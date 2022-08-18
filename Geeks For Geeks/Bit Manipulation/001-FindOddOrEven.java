a6ec9d0e5503d2fbc6c019ebacf77d2ef5adf85c
Subhampreet Mohanty <subhampreet18@gmail.com>
2022-08-18 21:48:33 +0530

    Bit Manipulation : 001 - Find Odd or Even

---
+++ Geeks For Geeks/Bit Manipulation/001-FindOddOrEven.java
@@ -0,0 +1,14 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+ 
+        if ((n & 1) != 0) {
+            System.out.println(n + " is odd");
+        }
+        else {
+            System.out.println(n + " is even");
+        }
+    }
+}
