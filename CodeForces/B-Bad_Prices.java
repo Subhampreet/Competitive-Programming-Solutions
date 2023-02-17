30ae7581a2f294db81a43e7346dbe8f083bb59c3
Subhampreet Mohanty <subhampreet18@gmail.com>
2023-02-17 23:41:29 +0530

    Codeforces 123B1

--- CodeForces/B-Bad_Prices.java
+++ CodeForces/B-Bad_Prices.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
  
-import java.util.Stack;
- 
- 
-public class HelloWorld{
- 
-     public static void main(String []args){
-         
-         Scanner sc = new Scanner(System.in);
-         
+/**
+ * <a href="https://codeforces.com/problemset/problem/1213/B"/>
+ */
+public class Problem1213B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-- >= 1){
+        while(t-->=1){
             int n = sc.nextInt();
             int prices[] = new int[n];
-            for(int i = 0 ; i < n ; i++){
+            for(int i =0; i<n;i++){
                 prices[i] = sc.nextInt();
             }
-            int count = 0;
-            
+            int countOfBadDays = 0;
             int min = prices[n-1];
-            
-            for(int i = n-2; i>=0; i--){
+            for(int i=n-2; i>=0;i--){
                 if(prices[i]>min){
-                    count++;
+                    //we have compared that price of today is greater than the minimum value
+                    countOfBadDays++;
                 }
                 min = Math.min(prices[i], min);
             }
-            System.out.println(count);
-        } 
-     }
+            System.out.println(countOfBadDays);
+        }
+ 
+    }
 }
