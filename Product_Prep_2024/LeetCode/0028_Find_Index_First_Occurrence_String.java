963f8fafa09f247b559013e3425378bbd7a8c0d8
Subhampreet Mohanty <subhampreet18@gmail.com>
2024-07-09 10:47:32 +0530

    PRD 4.1

---
+++ Product_Prep_2024/LeetCode/0028_Find_Index_First_Occurrence_String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty()){
+            return 0;
+        }
+
+        int haystackLength = haystack.length() ;
+        int needleLength = needle.length();
+
+        for(int i = 0; i <= haystackLength - needleLength; i++){
+            if(haystack.substring(i, i+needleLength).equals(needle)){
+                return i;
+            }
+        }
+
+        return -1;
+        
+    }
+}
