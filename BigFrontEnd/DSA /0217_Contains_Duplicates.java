f1677fd5158d63036d3f0e16b2facf254cc90451
Subhampreet Mohanty <subhampreet18@gmail.com>
2024-09-12 13:44:30 +0530

    BFE 2.1

---
+++ BigFrontEnd/DSA /0217_Contains_Duplicates.java
@@ -0,0 +1,33 @@
+// Solution 1 : Using HashSet in Java
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> numbers = new HashSet<>();
+
+//         for(int i = 0;i < nums.length; i++ ){
+//             if(numbers.contains(nums[i])) return true;
+//             numbers.add(nums[i]);
+//         }
+//         return false;
+//     }
+// }
+
+// Solution 2: Sort Array using Arrays.Sort in Java
+// Compare element in 1 & i+1
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+}
+
+// JavaScript Solution : 
+
+// var containsDuplicate = function(nums) {
+//     const set = new Set(nums);
+//     return set.size !== nums.length
+// };
