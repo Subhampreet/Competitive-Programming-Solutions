4bb0c5792156ba9e478e1a0da2d68bbeca502d91
Subhampreet Mohanty <subhampreet18@gmail.com>
2024-10-14 16:13:11 +0530

    Grind75_Week1_0.3

--- BigFrontEnd/DSA /0125_Valid_Palindrome.js
+++ BigFrontEnd/DSA /0125_Valid_Palindrome.js
@@ -1,31 +1,45 @@
+// Solution explanation - https://www.youtube.com/watch?v=P1gmutIKPRU
+
+
 var isPalindrome = function(s) {
+    let regex = /[^a-zA-Z0-9]/g
+    let clean = s.replace(regex, "")
 
-    const isAlphanumeric = (char) => {
-        return (char >= 'A' && char <= 'Z') || 
-               (char >= 'a' && char <= 'z') || 
-               (char >= '0' && char <= '9');
-    }
+    console.log(clean.toLowerCase())
+    console.log(clean.toLowerCase().split("").reverse().join())
 
-
-    let left = 0
-    let right = s.length - 1 
-
-    while( left < right){
-        while(left < right && !isAlphanumeric(s[left])){
-            left++;
-        }
-
-        while(left < right && !isAlphanumeric(s[right])){
-            right--;
-        }
-
-        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-            return false;
-        }
-
-        right--
-        left++        
-    }
-
-    return true
+    return clean.toLowerCase() === clean.toLowerCase().split("").reverse().join("")
 };
+
+
+// var isPalindrome = function(s) {
+
+//     const isAlphanumeric = (char) => {
+//         return (char >= 'A' && char <= 'Z') || 
+//                (char >= 'a' && char <= 'z') || 
+//                (char >= '0' && char <= '9');
+//     }
+
+
+//     let left = 0
+//     let right = s.length - 1 
+
+//     while( left < right){
+//         while(left < right && !isAlphanumeric(s[left])){
+//             left++;
+//         }
+
+//         while(left < right && !isAlphanumeric(s[right])){
+//             right--;
+//         }
+
+//         if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+//             return false;
+//         }
+
+//         right--
+//         left++        
+//     }
+
+//     return true
+// };
