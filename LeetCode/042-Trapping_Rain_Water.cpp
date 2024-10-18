67b7e15bb53b72324f08538e623de0e26d906ec8
Irfan Ansari <71096605+IRFANSARI@users.noreply.github.com>
2024-10-18 10:03:06 +0530

    Update 042-Trapping_Rain_Water.cpp

--- LeetCode/042-Trapping_Rain_Water.cpp
+++ LeetCode/042-Trapping_Rain_Water.cpp
@@ -1,38 +1,31 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) return 0;
 
-int trap(vector<int>& height) {
-    int n = height.size();
-    if (n == 0) return 0;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int trappedWater = 0;
 
-    int left = 0, right = n - 1;
-    int leftMax = 0, rightMax = 0;
-    int trappedWater = 0;
-
-    while (left <= right) {
-        if (height[left] <= height[right]) {
-            if (height[left] >= leftMax) {
-                leftMax = height[left];  // Update leftMax
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];  // Update leftMax
+                } else {
+                    trappedWater += leftMax - height[left];  // Water can be trapped
+                }
+                left++;
             } else {
-                trappedWater += leftMax - height[left];  // Water can be trapped
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];  // Update rightMax
+                } else {
+                    trappedWater += rightMax - height[right];  // Water can be trapped
+                }
+                right--;
             }
-            left++;
-        } else {
-            if (height[right] >= rightMax) {
-                rightMax = height[right];  // Update rightMax
-            } else {
-                trappedWater += rightMax - height[right];  // Water can be trapped
-            }
-            right--;
         }
+
+        return trappedWater;
     }
-
-    return trappedWater;
-}
-
-int main() {
-    vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-    cout << "Trapped Water: " << trap(height) << endl;
-    return 0;
-}
+};
